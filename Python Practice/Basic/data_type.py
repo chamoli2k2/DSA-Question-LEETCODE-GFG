9812eef5d6b58c73dc968ae918d6060d57b5ac6f
Chamoli <godchamoli@gmail.com>
2024-06-02 17:27:39 +0530

    feat: Implement swapNibbles, solve, and main functions for Bit manipulations/Swap two nibbles in a byte

---
+++ Python Practice/Basic/data_type.py
@@ -0,0 +1,27 @@
+"""
+This module provides examples of different data types in Python.
+"""
+
+# Your code goes here...
+
+mylist = [1, 2, 3, 4, 5, ["a", "b", "c", "d", "e"], ["apple", "banana", "cherry", "date", "elderberry"]]
+# for i in mylist:
+#     print(i)
+
+
+mytuple = (1, 2, 3, 4, 5, ("a", "b", "c", "d", "e"), ("apple", "banana", "cherry", "date", "elderberry"))
+# for i in mytuple:
+#     print(i)
+
+
+myset = {1, 2, 1, 1, 5, "a", "b", "c", "d", "e", "apple", "apple", "cherry", "date", "elderberry"}
+
+# for i in myset:
+#     print(i)
+
+myset = {5, 4, 3, 2, 2, 1, 1, 1, 1}
+# for i in myset:
+#     print(i)   
+
+myDictionay = {'one': 'lemon', 'two': 'hello', 1:'new'};
+print(myDictionay[1])
---
+++ Python Practice/Basic/hello_world.py
@@ -0,0 +1,7 @@
+# This is a simple hello world program in Python
+
+def hello_world(str):
+    print(str)
+
+
+hello_world("Hello world!")
